fd7010b8cae1859567766ea9c51ba183bf84f4fc
Adrien Patuzzo <A.pats@hotmail.fr>
2026-02-19 23:41:02 +0100

    Ajout des modèles bookings et resources

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from resources.models import Resource
 
 # Create your models here.
+#On efface pas la réservation, on change son statut.
+class Booking(models.Model):
+    STATUS_CHOICES = [
+        ('CONFIRMED', 'Confirmée'),
+        ('CANCELLED', 'Annulée'),
+    ]
+
+    # Une réservation concerne UNE ressource, une ressource peut avoir plusieurs réservations
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    #Une réservation appartient à UN utilisateur, un utilisateur peut faire plusieurs réservations
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #DateTimeField car dans le TP on parle de créneaux horaires.
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='CONFIRMED')
+    #rempli à la création
+    created_at = models.DateTimeField(auto_now_add=True)
+    # mis à jour à chaque modification
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.resource} - {self.start_date}"
--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+# Une catégorie permet de regrouper les ressources.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+# Important pour l'admin Django, permet de rendre l'affichage lisible
+    def __str__(self):
+        return self.name
+
+# Même logique que pour catégorie, une localisation peut représenter batiment / salle / entrepot / etc...
+class Location(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+# limite des valeurs possibles, première valeur sotckée en base, deuxième valeur affichée a l'utilisateur.
+class Resource(models.Model):
+    RESOURCE_TYPES = [
+        ('ROOM', 'Salle'),
+        ('EQUIPMENT', 'Équipement'),
+    ]
+
+    #Nom de la ressource
+    name = models.CharField(max_length=150)
+    #Doit être >= longeur de l'equipement, Impose le menu déroulant
+    type = models.CharField(max_length=20, choices=RESOURCE_TYPES)
+    #Une ressource appartient à UNE catégorie, une catégorie peut avoir plusieurs ressources, si la catégorie est supprimée, ses ressources aussi
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name

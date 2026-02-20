e69577354126612543d260ef94c6f31a8a461e32
Adrien Patuzzo <A.pats@hotmail.fr>
2026-02-20 05:11:36 +0100

    feat: création réservations + vue réservations suite

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import BookingCreateView, MyBookingsView
+from . import views
 
 urlpatterns = [
     path("create/", BookingCreateView.as_view(), name="booking_create"),
     path("my/", MyBookingsView.as_view(), name="my_bookings"),
+    path("cancel/<int:booking_id>/", views.cancel_booking, name="cancel_booking"),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -4,6 +4,9 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Booking
 from .forms import BookingForm
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -25,3 +28,23 @@ class MyBookingsView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Booking.objects.filter(user=self.request.user)
+
+
+@login_required
+def cancel_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+
+    # Sécurité : seul le propriétaire ou un admin peut annuler
+    if booking.user != request.user and not request.user.groups.filter(name="ADMIN").exists():
+        messages.error(request, "Vous n'avez pas le droit d'annuler cette réservation.")
+        return redirect("my_bookings")
+
+    if booking.status == "CANCELLED":
+        messages.warning(request, "Cette réservation est déjà annulée.")
+        return redirect("my_bookings")
+
+    booking.status = "CANCELLED"
+    booking.save()
+
+    messages.success(request, "Réservation annulée avec succès.")
+    return redirect("my_bookings")
